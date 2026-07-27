5f7b8a04a6af5a497dfd4ac404a6a313789a6582
Nastya <anastasion0107@gmail.com>
2026-07-27 20:23:07 +0700

    tailwind install and setting

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+    return (
+        <>
+            <h1 className="underline text-amber-800">Hello</h1>
+            <div className="flex gap-2">
+                <p>Попка</p>
+                <p>Жопка</p>
+            </div>
+        </>
+    )
+}
+
+export default App
